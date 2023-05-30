ddf6b98a84d56295c3a01dce22888a013a38da92
Irene Lopez <elcorreodeirene@gmail.com>
2023-05-30 18:39:01 +0200

    style: added css and styled components

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { CartContext } from "../context/cart";
+import styled from "styled-components";
 
 const Cart = () => {
   const { selectedProducts, setSelectedProducts } = useContext(CartContext);
@@ -14,28 +15,42 @@ const Cart = () => {
   };
   return (
     <>
-      <h1>Cart</h1>
-      <p>Products in cart: {selectedProducts.length}</p>
-      <table>
+      <h2>Cart</h2>
+      <H3>Products in cart: {selectedProducts.length}</H3>
+      <Table>
         {selectedProducts.length <= 0
           ? "No hay nada en el carrito"
           : selectedProducts.map((product) => (
               <tr key={product.id}>
-                <td>{product.model}</td>
-                <td>{product.brand}</td>
-                <td>{product.price}€</td>
-                <td>{product.quantity}Uds</td>
-                <td>
+                <Td>{product.model}</Td>
+                <Td>{product.brand}</Td>
+                <Td>{product.price}€</Td>
+                <Td>{product.quantity}Uds</Td>
+                <Td>
                   <button onClick={() => handleDelete(product.id)}>
                     Delete
                   </button>
-                </td>
+                </Td>
               </tr>
             ))}
-      </table>
+      </Table>
       <h3>Total: {total}€</h3>
     </>
   );
 };
 
 export default Cart;
+
+const H3 = styled.h3`
+  margin: 0.8rem 0;
+  color: #212a3e;
+`;
+
+const Table = styled.table`
+  background-color: #9ba4b5;
+  margin: 2rem 0;
+`;
+
+const Td = styled.td`
+  padding: 1rem;
+`;
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ProductsContext } from "../context/products";
 import { singleProduct } from "../mocks/singleProduct.json";
 import { CartContext } from "../context/cart";
+import styled from "styled-components";
 
 const ProductDetail = () => {
   const { selectedProducts, setSelectedProducts } = useContext(CartContext);
@@ -23,28 +24,98 @@ const ProductDetail = () => {
   };
 
   return (
-    <div>
-      <img scr={singleProduct.imgUrl} alt={singleProduct.model}></img>
-      <h3>{selectedProduct[0].model}</h3>
-      <h3>Brand: {selectedProduct[0].brand}</h3>
-      <h3>Price: {selectedProduct[0].price}€</h3>
-      <p>CPU: {singleProduct.cpu}</p>
-      <p>RAM: {singleProduct.ram}</p>
-      <p>OS: {singleProduct.os[0]}</p>
-      <p>Display Resolution: {singleProduct.displayResolution}</p>
-      <p>Battery: {singleProduct.battery}</p>
-      <p>
-        Cameras:
-        <span>Primary Camera: {singleProduct.primaryCamera}</span>
-        <span>Second Camera: {singleProduct.secondaryCmera}</span>
-      </p>
-      <p>Dimentions: {singleProduct.dimentions}</p>
-      <p>Weight: {singleProduct.weight}</p>
-      <button onClick={() => handleAddToCart(selectedProduct[0].id)}>
-        Add to Cart
-      </button>
-    </div>
+    <>
+      <Button>
+        <Link to={"/"}>Back to all Products</Link>
+      </Button>
+      <Container>
+        <Img
+          src="https://i.dummyjson.com/data/products/2/1.jpg"
+          alt={singleProduct.model}
+        ></Img>
+        <section>
+          <Div>
+            <H3>{selectedProduct[0].model}</H3>
+            <H3>Brand: {selectedProduct[0].brand}</H3>
+            <H3>Price: {selectedProduct[0].price}€</H3>
+            <P>CPU: {singleProduct.cpu}</P>
+            <P>RAM: {singleProduct.ram}</P>
+            <P>OS: {singleProduct.os[0]}</P>
+            <P>Display Resolution: {singleProduct.displayResolution}</P>
+            <P>Battery: {singleProduct.battery}</P>
+            <P>
+              Cameras:
+              <span>Primary Camera: {singleProduct.primaryCamera}</span>
+              <span>Second Camera: {singleProduct.secondaryCmera}</span>
+            </P>
+            <P>Dimentions: {singleProduct.dimentions}</P>
+            <P>Weight: {singleProduct.weight}</P>
+          </Div>
+          <Div>
+            <H3>Select your favorites options</H3>
+            <Select>
+              {singleProduct.options.colors.map((option) => (
+                <option key={option.code}>{option.name}</option>
+              ))}
+            </Select>
+            <Select>
+              {singleProduct.options.storages.map((option) => (
+                <option key={option.code}>{option.name}</option>
+              ))}
+            </Select>
+            <Button onClick={() => handleAddToCart(selectedProduct[0].id)}>
+              <Link to={"/"}>Add to Cart</Link>
+            </Button>
+          </Div>
+        </section>
+      </Container>
+    </>
   );
 };
 
 export default ProductDetail;
+
+const Container = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 3rem;
+  align-items: flex-start;
+  margin-top: 2rem;
+`;
+const H3 = styled.h3`
+  margin: 0.8rem 0;
+  color: #212a3e;
+`;
+const P = styled.p`
+  margin: 0.5rem 0;
+  color: #212a3e;
+`;
+const Img = styled.img`
+  max-width: 100%;
+  border-radius: 1rem;
+`;
+
+const Div = styled.div`
+  background-color: whitesmoke;
+  border: solid 3px #9ba4b5;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 1rem;
+  border-radius: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const Select = styled.select`
+  margin-bottom: 0.8rem;
+  padding: 0.5rem;
+`;
+
+const Button = styled.button`
+  background-color: #9ba4b5;
+  color: whitesmoke;
+
+  &:hover {
+    background-color: whitesmoke;
+    color: #212a3e;
+  }
+`;
