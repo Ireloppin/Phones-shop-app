598e0c950766158c0fc02e187c757b66888c9cca
Irene Lopez <elcorreodeirene@gmail.com>
2023-06-05 10:29:57 +0200

    fix: change localStorage cache

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -3,16 +3,41 @@ import React, { createContext, useEffect, useState } from "react";
 export const CartContext = createContext();
 
 export function CartProvider({ children }) {
+  const hours = 1;
+  const now = new Date().getTime();
+  var setupTime = localStorage.getItem("setupTime");
+  if (setupTime == null) {
+    localStorage.setItem("setupTime", now);
+  } else {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
+      localStorage.removeItem("products");
+      localStorage.setItem("setupTime", now);
+    }
+  }
+
   const cartInitialState = JSON.parse(localStorage.getItem("products")) || [];
 
   const [selectedProducts, setSelectedProducts] = useState(cartInitialState);
 
   useEffect(() => {
     localStorage.setItem("products", JSON.stringify(selectedProducts));
-    clearTimeout();
+
+    /*   const hours = 1;
+    const now = new Date().getTime();
+    var setupTime = localStorage.getItem("setupTime");
+    if (setupTime == null) {
+      localStorage.setItem("setupTime", now);
+    } else {
+      if (now - setupTime > hours * 60 * 60) {
+        localStorage.removeItem("products");
+        localStorage.setItem("setupTime", now);
+      }
+    } */
+
+    /*  clearTimeout();
     setTimeout(() => {
       localStorage.removeItem("products");
-    }, 1000 * 60 * 60);
+    }, 1000 * 60 * 60); */
   }, [selectedProducts]);
 
   return (
